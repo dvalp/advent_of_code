7060291991db3e12a5a7004ba2fa71de525f2798
David Valpey <david.valpey@viqtordavis.com>
2020-12-03 11:03:12 +0100

    Messy approach to counting trees

--- 2020/day03_solution.py
+++ 2020/day03_solution.py
@@ -1,3 +1,7 @@
+import math
+from typing import Iterable
+
+
 def tree_count():
     horiz = 0
 
@@ -8,5 +12,43 @@ def tree_count():
             yield line[horiz % 31] == '#'
 
 
+def tree_count_deluxe(
+        tree_map: list[str],
+        start_coords: tuple[int, int] = (0, 0),
+        offsets: tuple[int, int] = (1, 1)
+) -> Iterable[bool]:
+    horiz_offset, vert_offset = offsets
+    vert, horiz = start_coords
+    width = len(tree_map[0])
+    while vert < len(tree_map):
+        yield tree_map[vert][horiz % width] == "#"
+        horiz += horiz_offset
+        vert += vert_offset
+
+
+def check_all_slopes(tree_map: list[str]) -> int:
+    route_offsets = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    return math.prod(sum(tree_count_deluxe(tree_map=tree_map, offsets=offsets)) for offsets in route_offsets)
+
+
+def parse_input(fpath="data/input_day03.txt") -> list[str]:
+    with open(fpath, "r") as f:
+        return [line.strip() for line in f]
+
+
 if __name__ == '__main__':
-    sum(tree_count())
+    print(sum(tree_count()))
+    t_map = """..##.......\n
+    #...#...#..\n
+    .#....#..#.\n
+    ..#.#...#.#\n
+    .#...##..#.\n
+    ..#.##.....\n
+    .#.#.#....#\n
+    .#........#\n
+    #.##...#...\n
+    #...##....#\n
+    .#..#...#.#\n""".split()
+    print(check_all_slopes(t_map))
+    print(sum(tree_count_deluxe(tree_map=parse_input(), offsets=(3, 1))))
+    print(check_all_slopes(parse_input()))
