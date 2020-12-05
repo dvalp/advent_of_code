9ed49b7241df1c1aa8b30f79e84c642e982e638a
David Valpey <david.valpey@viqtordavis.com>
2020-12-05 10:11:02 +0100

    Day 5 set up

---
+++ 2020/src/day05_solution.py
@@ -0,0 +1,18 @@
+from collections import namedtuple
+
+SampleID = namedtuple("SampleID", "code row column id")
+SAMPLES = [
+    SampleID("FBFBBFFRLR", 44, 5, 357),
+    SampleID("BFFFBBFRRR", 70, 7, 567),
+    SampleID("FFFBBBFRRR", 14, 7, 119),
+    SampleID("BBFFBBFRLL", 102, 4, 820),
+
+]
+
+def seat_finder_01(boarding_pass: str):
+    rows = 128
+    seats = 8
+    row_code = boarding_pass[:7]
+    column_code = boarding_pass[7:]
+
+    seat_id = row * 8 + column
