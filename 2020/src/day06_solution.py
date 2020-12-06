c4c4000d37a421bf425e930a3c8d6ffd38db5fc3
David Valpey <david.valpey@viqtordavis.com>
2020-12-06 09:41:21 +0100

    Type hinting and docstrings

--- 2020/src/day06_solution.py
+++ 2020/src/day06_solution.py
@@ -3,17 +3,19 @@ from pathlib import Path
 from typing import Callable
 
 
-def total_answers(func: Callable):
+def total_answers(func: Callable) -> int:
     fpath = Path("../data/input_day06.txt")
     answers = fpath.read_text().split("\n\n")
     return sum(func(answer) for answer in answers)
 
 
-def count_group(answer):
+def count_group(answer: str) -> int:
+    """Count answers by anyone in the group"""
     return len(set(answer.replace("\n", "")))
 
 
-def count_group_yes(answer):
+def count_group_yes(answer: str) -> int:
+    """Count only answers given by all members of the group"""
     group_size = len(answer.split())
     counts = Counter(answer.replace("\n", ""))
     return sum(True for val in counts.values() if val == group_size)
