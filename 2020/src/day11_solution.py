c6f952dc445d8e5395bf4c52ba3a1c8129445a27
David Valpey <david.valpey@viqtordavis.com>
2020-12-11 12:06:04 +0100

    Initialize new board

--- 2020/src/day11_solution.py
+++ 2020/src/day11_solution.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
 from enum import Enum
+from pathlib import Path
+from typing import NamedTuple
 
 
 class State(Enum):
-    EMPTY = "L"
-    FILLED = "#"
-    FLOOR = "."
+    EMPTY = 0
+    FILLED = 1
+    FLOOR = None
 
 
 class Action(Enum):
@@ -13,19 +15,48 @@ class Action(Enum):
     EMPTY = 4
 
 
-@dataclass
-class Seat:
-    value: State
+class Point(NamedTuple):
     row: int
     column: int
 
 
 @dataclass
-class Board:
-    map: list[list[str]]
+class Seat:
+    location: Point
+    status: State
 
-    def initialize_board(self):
-        pass
+
+@dataclass
+class Board:
+    seat_map: dict[Point, State]
+
+    @staticmethod
+    def initialize_board(empty_map: str):
+        """All seats start empty, so populate the dictionary with only empty seats"""
+        new_map = {}
+        current_location = Point(0, 0)
+        for c in empty_map:
+            if c == "\n":
+                current_location = Point(current_location.row + 1, 0)
+            if c == "L":
+                new_map[current_location] = State.EMPTY
+                current_location = Point(current_location.row, current_location.column + 1)
+        return Board(new_map)
 
     def update_board(self):
         pass
+
+
+if __name__ == '__main__':
+    sample_map = """L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL"""
+    challenge_map = Path("../data/input_day11.txt").read_text()
+    sample_board = Board.initialize_board(sample_map)
