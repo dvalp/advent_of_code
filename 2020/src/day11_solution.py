ed173eb8ef0c04b553b9231f9fdab1d03c9dd48a
David Valpey <david.valpey@viqtordavis.com>
2020-12-11 12:54:50 +0100

    Count neighbors and create a new seat map

--- 2020/src/day11_solution.py
+++ 2020/src/day11_solution.py
@@ -1,16 +1,17 @@
 from dataclasses import dataclass
-from enum import Enum
+from enum import IntEnum
+from itertools import product
 from pathlib import Path
 from typing import NamedTuple
 
 
-class State(Enum):
+class State(IntEnum):
     EMPTY = 0
     FILLED = 1
-    FLOOR = None
+    FLOOR = -1
 
 
-class Action(Enum):
+class Action(IntEnum):
     FILL = 0
     EMPTY = 4
 
@@ -18,12 +19,6 @@ class Action(Enum):
 class Point(NamedTuple):
     row: int
     column: int
-
-
-@dataclass
-class Seat:
-    location: Point
-    status: State
 
 
 @dataclass
@@ -43,8 +38,21 @@ class Board:
             current_location = Point(current_location.row, current_location.column + 1)
         return Board(new_map)
 
+    def count_neighbors(self, seat_position: Point):
+        rows = range(seat_position.row - 1, seat_position.row + 2)
+        columns = range(seat_position.column - 1, seat_position.column + 2)
+        search_space = {Point(*coord) for coord in product(rows, columns) if coord != seat_position}
+        return sum(self.seat_map.get(neighbor, State.EMPTY) for neighbor in search_space)
+
     def update_board(self):
-        pass
+        new_map = dict()
+        for seat in self.seat_map:
+            number_neighbors = self.count_neighbors(seat)
+            if number_neighbors == Action.FILL:
+                new_map[seat] = State.FILLED
+            if number_neighbors >= Action.EMPTY:
+                new_map[seat] = State.EMPTY
+        return Board(new_map)
 
 
 if __name__ == '__main__':
@@ -60,3 +68,7 @@ L.LLLLLL.L
 L.LLLLL.LL"""
     challenge_map = Path("../data/input_day11.txt").read_text()
     sample_board = Board.initialize_board(sample_map)
+    print(sample_board.count_neighbors(Point(0, 2)))
+    print(sample_board)
+    print(sample_board.update_board())
+    print(sum(sample_board.update_board().seat_map.values()))
