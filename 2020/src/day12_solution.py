540cfb6a886520c914b3e13df857b44d3c06549b
David Valpey <david.valpey@viqtordavis.com>
2020-12-12 11:06:20 +0100

    Rotate waypoint

--- 2020/src/day12_solution.py
+++ 2020/src/day12_solution.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from enum import IntEnum
 from pathlib import Path
+from typing import Literal
 
 
 class Direction(IntEnum):
@@ -12,30 +13,43 @@ class Direction(IntEnum):
 
 @dataclass
 class Navigator:
-    long_dist: int = 0
-    lat_dist: int = 0
+    ship_long: int = 0
+    ship_lat: int = 0
+    wp_lat: int = 10
+    wp_long: int = 1
     facing: IntEnum = Direction.E
 
-    def read_instructions(self, instructions: list[str]) -> None:
+    @property
+    def manhattan_dist(self) -> int:
+        return abs(self.ship_lat) + abs(self.ship_long)
+
+    def read_instructions(self, instructions: list[str], move: Literal["normal", "waypoint"] = "normal") -> None:
         dirs = {"N": Direction.N, "E": Direction.E, "S": Direction.S, "W": Direction.W}
+        if move == "normal":
+            move_function = self.move_direction
+            turn_function = self.change_direction
+        else:
+            move_function = self.move_waypoint
+            turn_function = self.rotate_waypoint
+
         for line in instructions:
             command, value = line[0], int(line[1:])
             if command == "F":
-                self.move_direction(self.facing, value)
+                move_function(direction=self.facing, dist=value)
             elif command in set("NESW"):
-                self.move_direction(dirs[command], value)
+                move_function(dirs[command], value)
             elif command in set("LR"):
-                self.change_direction(command, value)
+                turn_function(command, value)
 
     def move_direction(self, direction: IntEnum, dist: int) -> None:
         if direction == Direction.E:
-            self.lat_dist += dist
+            self.ship_lat += dist
         elif direction == Direction.N:
-            self.long_dist += dist
+            self.ship_long += dist
         elif direction == Direction.W:
-            self.lat_dist -= dist
+            self.ship_lat -= dist
         elif direction == Direction.S:
-            self.long_dist -= dist
+            self.ship_long -= dist
         else:
             raise ValueError("slef.facing only has 4 allowed vallues. %s is invalid." % direction)
 
@@ -49,9 +63,18 @@ class Navigator:
             raise ValueError("Ship can only turn L or R")
         self.facing = Direction(degrees)
 
-    @property
-    def manhattan_dist(self) -> int:
-        return abs(self.lat_dist) + abs(self.long_dist)
+    def move_waypoint(self, direction: IntEnum, dist: int):
+        pass
+
+    def rotate_waypoint(self, command: str, value: int):
+        if (command == "L" and value == 90) or (command == "R" and value == 270):
+            self.wp_lat, self.wp_long = -self.wp_long, self.wp_lat
+        elif (command == "R" and value == 90) or (command == "L" and value == 270):
+            self.wp_lat, self.wp_long = self.wp_long, -self.wp_lat
+        elif value == 180:
+            self.wp_lat, self.wp_long = -self.wp_lat, -self.wp_long
+        else:
+            raise ValueError("Turn instruction or value was invalid")
 
 
 if __name__ == '__main__':
