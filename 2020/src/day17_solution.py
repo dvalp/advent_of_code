e7b696e80744b9024f6f5fd26f5762ad81cdbafc
David Valpey <david.valpey@viqtordavis.com>
2020-12-17 23:17:49 +0100

    Day 17 refactor classes to add 4th dimension

--- 2020/src/day17_solution.py
+++ 2020/src/day17_solution.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+
 from collections import deque
 from dataclasses import dataclass, field
 from typing import NamedTuple
@@ -25,22 +26,34 @@ class Coord(NamedTuple):
         return Coord(self.x + other.x, self.y + other.y, self.z + other.z)
 
 
+class Coord4D(NamedTuple):
+    x: int
+    y: int
+    z: int
+    w: int
+
+    def __add__(self, other):
+        return Coord4D(self.x + other.x, self.y + other.y, self.z + other.z, self.w + other.w)
+
+
 @dataclass
 class PowerGrid:
     active_cells: set[Coord] = field(default_factory=set)
     neighbor_references: set[Coord] = field(default_factory=set)
 
-    def __init__(self, grid_map: str):
-        self.active_cells = set()
+    @staticmethod
+    def parse_data(grid_map: str):
+        active_cells = set()
         for y, row in enumerate(grid_map.split()):
             for x, value in enumerate(row):
                 new_coord = Coord(x, y, 0)
                 if value == "#":
-                    self.active_cells.add(new_coord)
+                    active_cells.add(new_coord)
 
         ints = {-1, 0, 1}
-        self.neighbor_references = {Coord(x, y, z) for x in ints for y in ints for z in ints
-                                    if not (x, y, z) == (0, 0, 0)}
+        neighbor_references = {Coord(x, y, z) for x in ints for y in ints for z in ints
+                               if not (x, y, z) == (0, 0, 0)}
+        return PowerGrid(active_cells=active_cells, neighbor_references=neighbor_references)
 
     @property
     def active_count(self):
@@ -73,9 +86,29 @@ class PowerGrid:
         print(self.active_count)
 
 
+@dataclass
+class PowerGrid4D(PowerGrid):
+    active_cells: set[Coord4D] = field(default_factory=set)
+    neighbor_references: set[Coord4D] = field(default_factory=set)
+
+    @staticmethod
+    def parse_data(grid_map: str):
+        active_cells = set()
+        for y, row in enumerate(grid_map.split()):
+            for x, value in enumerate(row):
+                new_coord = Coord4D(x, y, 0, 0)
+                if value == "#":
+                    active_cells.add(new_coord)
+
+        ints = {-1, 0, 1}
+        neighbor_references = {Coord4D(x, y, z, w) for x in ints for y in ints for z in ints for w in ints
+                               if not (x, y, z, w) == (0, 0, 0, 0)}
+        return PowerGrid4D(active_cells=active_cells, neighbor_references=neighbor_references)
+
+
 if __name__ == '__main__':
     # test_grid = PowerGrid(TEST_DATA)
     # test_grid.run_cycles(6)
 
-    true_grid = PowerGrid(CHALLENGE_DATA)
+    true_grid = PowerGrid.parse_data(CHALLENGE_DATA)
     true_grid.run_cycles(6)
