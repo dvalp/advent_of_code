4729f9981a1b5341b2e799f0498851472cd7647e
David Valpey <david.valpey@viqtordavis.com>
2021-12-01 08:18:18 +0100

    Brute force day 1

---
+++ 2021/src/day01_solution.py
@@ -0,0 +1,32 @@
+SAMPLE_DATA = """199
+200
+208
+210
+200
+207
+240
+269
+260
+263"""
+
+
+def count_increased_depth():
+    with open("data/input_day01.txt", "r") as f:
+        count = 0
+        prev = int(next(f))
+        for line in f:
+            curr = int(line)
+            if curr > prev:
+                count += 1
+            prev = curr
+    return count
+
+
+def count_sliding_depth():
+    with open("data/input_day01.txt", "r") as f:
+        data = [int(value) for value in f]
+    count = 0
+    for idx in range(len(data) - 3):
+        if sum(data[idx:idx + 3]) < sum(data[idx + 1:idx + 4]):
+            count += 1
+    return count
