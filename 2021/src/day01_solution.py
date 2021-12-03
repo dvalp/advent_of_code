3ceb0e0ccb0d644d2b34fe70d4dda762ec77e737
David Valpey <david.valpey@viqtordavis.com>
2021-12-03 23:33:10 +0100

    Add alternate solution

--- 2021/src/day01_solution.py
+++ 2021/src/day01_solution.py
@@ -11,7 +11,7 @@ SAMPLE_DATA = """199
 
 
 def count_increased_depth():
-    with open("data/input_day01.txt", "r") as f:
+    with open("../data/input_day01.txt", "r") as f:
         count = 0
         prev = int(next(f))
         for line in f:
@@ -23,10 +23,28 @@ def count_increased_depth():
 
 
 def count_sliding_depth():
-    with open("data/input_day01.txt", "r") as f:
+    with open("../data/input_day01.txt", "r") as f:
         data = [int(value) for value in f]
     count = 0
     for idx in range(len(data) - 3):
         if sum(data[idx:idx + 3]) < sum(data[idx + 1:idx + 4]):
             count += 1
     return count
+
+
+def calculate_with_gap(gap: int = 1):
+    """With thanks to Joel Grus for the idea"""
+    with open("../data/input_day01.txt", "r") as f:
+        data = [int(value) for value in f]
+    count = 0
+    for idx in range(len(data) - gap):
+        if data[idx] < data[idx + gap]:
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    print(count_increased_depth())
+    print(calculate_with_gap(1))
+    print(count_sliding_depth())
+    print(calculate_with_gap(3))
