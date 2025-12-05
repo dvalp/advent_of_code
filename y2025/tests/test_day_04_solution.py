456756d077088b6ad1732f5473a1878bfd7f02f8
David Valpey <greengargoyle@hotmail.com>
2025-12-05 02:03:59 +0100

    Add test Day 4

--- y2025/tests/test_day_04_solution.py
+++ y2025/tests/test_day_04_solution.py
@@ -1,6 +1,7 @@
+import numpy as np
 import pytest
 
-from y2025.src.day_04_solution import count_roles, char_translation
+from y2025.src.day_04_solution import convert_data, count_roles
 
 
 @pytest.fixture
@@ -20,3 +21,20 @@ def sample_input():
 def test_count_roles(sample_input):
     result = count_roles(sample_input)
     assert result == 13
+
+
+def test_convert_data(sample_input):
+    result = convert_data(sample_input)
+    expected = np.array(
+        [[0, 0, 1, 1, 0, 1, 1, 1, 1, 0],
+         [1, 1, 1, 0, 1, 0, 1, 0, 1, 1],
+         [1, 1, 1, 1, 1, 0, 1, 0, 1, 1],
+         [1, 0, 1, 1, 1, 1, 0, 0, 1, 0],
+         [1, 1, 0, 1, 1, 1, 1, 0, 1, 1],
+         [0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+         [0, 1, 0, 1, 0, 1, 0, 1, 1, 1],
+         [1, 0, 1, 1, 1, 0, 1, 1, 1, 1],
+         [0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+         [1, 0, 1, 0, 1, 1, 1, 0, 1, 0]]
+    )
+    assert (result == expected).all()
